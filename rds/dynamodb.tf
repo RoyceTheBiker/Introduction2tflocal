47ac6b3ea578c5643e1a552f2809c14ebc981e12
Royce Souther <osgnuru@gmail.com>
2024-12-15 14:21:49 -0700

    👌 IMPROVE: Terraform work

--- rds/dynamodb.tf
+++ rds/dynamodb.tf
@@ -1,10 +1,9 @@
 resource "aws_dynamodb_table" "basic-dynamodb-table" {
   name           = "Music"
-  # billing_mode   = "PROVISIONED"
-  # read_capacity  = 20
-  # write_capacity = 20
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 20
+  write_capacity = 20
   hash_key       = "TrackId"
-  # range_key      = "GameTitle"
 
   attribute {
     name = "TrackId"
