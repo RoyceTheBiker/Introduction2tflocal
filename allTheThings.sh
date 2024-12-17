32e76d067c79e688f25ce8506c7fd2273c4b82c2
Royce Souther <osgnuru@gmail.com>
2024-12-16 23:24:53 -0700

    👌 IMPROVE: Terraform work

--- allTheThings.sh
+++ allTheThings.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-[ $(id -u) -ne 1 ] && {
+[ $(id -u) -ne 0 ] && {
   echo "Please run as root"
   echo "sudo $0"
   exit
@@ -12,20 +12,20 @@ echo "Setup as root"
 ./setup.sh
 
 echo "Setup as user"
-su - ${SUDO_USER} -c ./setupAsUser.sh
+su ${SUDO_USER} -c ./setupAsUser.sh
 
 echo "Add color to Docker"
 ./dockerColor.sh
 eval $(grep ^PATH ~/.bashrc) # Load the PATH value
 
 echo "Create AWS credentials"
-su - ${SUDO_USER} -c ./awsCredentials.sh
+su ${SUDO_USER} -c ./awsCredentials.sh
 
 echo "Test LocalStack"
-su - ${SUDO_USER} -c ./testLocalStack.sh
+su ${SUDO_USER} -c ./testLocalStack.sh
 
 echo "Run Terraform"
-su - ${SUDO_USER} -c ./terraform.sh
+su ${SUDO_USER} -c ./terraform.sh
 
 echo "Test DynamoDB"
-su - ${SUDO_USER} -c ./testDynamoDb.sh
+su ${SUDO_USER} -c ./testDynamoDb.sh
--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,16 +1,12 @@
 resource "aws_instance" "linux" {
 	ami                         = "ami-0b301ce3ce347599c"
 	associate_public_ip_address = true
-	# depends_on 									= [ var.rds_endpoints	]
+	depends_on 									= [ var.roles	]
 	disable_api_termination     = false
-	# ebs_optimized               =
 	iam_instance_profile				= "S3_Access"
 	instance_type               = "t2.micro"
-	# key_name                    = aws_key_pair.ssh-key.key_name
 	monitoring                  = false
 	private_ip                  = "10.1.2.3"
-	# subnet_id                   = var.vpc_resources.elb_subnet.id
-	# vpc_security_group_ids			= [ var.security_group.id ]
 	source_dest_check           = true
 
 	root_block_device {
